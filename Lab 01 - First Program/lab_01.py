4475d289674f8b8eddafbae0f03d44dc82ef813b
Hannah Brewer <hannah.brewer@my.simpson.edu>
2021-09-02 15:18:38 -0500

    Work on Lab 01

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("I have three cats.")
+print("Their names are \"Millie\" \"Mollie\" and \"Dahlia.\" ")
+print("We found Dahlia in the woods, and claimed her as our own, though she still lives outside.")
+print("Millie and Mollie are sisters who have lived in our house for seven years.")

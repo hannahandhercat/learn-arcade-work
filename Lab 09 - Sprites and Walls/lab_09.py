529d239ff89e4e356819260e066eee54e8d09c8c
Hannah Brewer <hannah.brewer@my.simpson.edu>
2021-11-11 09:56:42 -0600

    Work on lab 09

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -2,6 +2,7 @@ import random
 import arcade
 
 SPRITE_SCALING = 0.5
+BIG_ROCK_SCALING = 1
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,13 +12,14 @@ VIEWPOINT_MARGIN = 220
 
 CAMERA_SPEED = 0.1
 
-PLAYER_MOVEMENT_SPEED = 7
+PLAYER_MOVEMENT_SPEED = 5
 
 TEXTURE_LEFT = 0
 TEXTURE_RIGHT = 1
 
 
 class Slime(arcade.Sprite):
+    """Create the class "Slime" for the player sprite."""
 
     def __init__(self):
         super().__init__()
@@ -25,6 +27,7 @@ class Slime(arcade.Sprite):
         self.scale = SPRITE_SCALING
         self.textures = []
 
+        # Create the textures for the player sprite to dictate which way the sprite faces.
         texture = arcade.load_texture("slimeBlue.png")
         self.textures.append(texture)
 
@@ -34,15 +37,11 @@ class Slime(arcade.Sprite):
         self.texture = texture
 
     def update(self):
-
+        """Update so that the player faces right or left when moving right or left."""
         if self.change_x < 0:
             self.texture = self.textures[TEXTURE_LEFT]
         elif self.change_x > 0:
             self.texture = self.textures[TEXTURE_RIGHT]
-
-
-class Gem(arcade.Sprite):
-    pass
 
 
 class MyGame(arcade.Window):
@@ -58,6 +57,9 @@ class MyGame(arcade.Window):
 
         # Set up the player
         self.player_sprite = None
+        self.score = 0
+
+        self.gem_sound = arcade.load_sound("get_gem.wav")
 
         # Physics engine so we don't run into walls.
         self.physics_engine = None
@@ -73,14 +75,15 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.gem_list = arcade.SpriteList()
 
         # Set up the player
         self.player_sprite = Slime()
-        self.player_sprite.center_x = 256
-        self.player_sprite.center_y = 512
+        self.player_sprite.center_x = 200
+        self.player_sprite.center_y = 200
         self.player_list.append(self.player_sprite)
 
-        # Saw Barrier
+        # Create a barrier of saws.
         for item in range(175, 1200, 50):
             saw = arcade.Sprite("sawHalf.png", SPRITE_SCALING)
             saw.center_x = item
@@ -102,7 +105,7 @@ class MyGame(arcade.Window):
             saw.center_y = item
             self.wall_list.append(saw)
 
-        # Bottom Row
+        # Bottom row of slime blocks.
         for x in range(200, 500, 50):
             slime_wall = arcade.Sprite("slimeBlock.png", SPRITE_SCALING)
             slime_wall.center_x = x
@@ -119,7 +122,7 @@ class MyGame(arcade.Window):
             slime_wall.center_y = 250
             self.wall_list.append(slime_wall)
 
-        # Next Row
+        # Slime blocks directly above the bottom row of slime blocks.
         for x in range(200, 600, 50):
             slime_wall = arcade.Sprite("slimeBlock.png", SPRITE_SCALING)
             slime_wall.center_x = x
@@ -131,7 +134,7 @@ class MyGame(arcade.Window):
             slime_wall.center_y = 400
             self.wall_list.append(slime_wall)
 
-        # Next Row
+        # Row of slime blocks underneath the top row of slime blocks.
         for x in range(200, 450, 50):
             slime_wall = arcade.Sprite("slimeBlock.png", SPRITE_SCALING)
             slime_wall.center_x = x
@@ -148,7 +151,7 @@ class MyGame(arcade.Window):
             slime_wall.center_y = 550
             self.wall_list.append(slime_wall)
 
-        # Top Row
+        # Top row of slime blocks.
         for x in range(200, 700, 50):
             slime_wall = arcade.Sprite("slimeBlock.png", SPRITE_SCALING)
             slime_wall.center_x = x
@@ -165,12 +168,93 @@ class MyGame(arcade.Window):
             slime_wall.center_y = 700
             self.wall_list.append(slime_wall)
 
+        # Create various barrier rocks.
+        rock = arcade.Sprite("meteorGrey_big1.png", SPRITE_SCALING)
+        rock.center_x = 700
+        rock.center_y = 190
+        self.wall_list.append(rock)
+
+        rock = arcade.Sprite("meteorGrey_big1.png", SPRITE_SCALING)
+        rock.center_x = 800
+        rock.center_y = 750
+        self.wall_list.append(rock)
+
+        rock = arcade.Sprite("meteorGrey_big1.png", BIG_ROCK_SCALING)
+        rock.center_x = 525
+        rock.center_y = 610
+        self.wall_list.append(rock)
+
+        # Create gems and place them.
+        for i in range(3):
+            gem = arcade.Sprite("gemBlue.png", SPRITE_SCALING)
+            gem_placed_successfully = False
+            while not gem_placed_successfully:
+                gem.center_x = random.randrange(250, 1150)
+                gem.center_y = 200
+                wall_hit_list = arcade.check_for_collision_with_list(gem, self.wall_list)
+                gem_hit_list = arcade.check_for_collision_with_list(gem, self.gem_list)
+                if len(wall_hit_list) == 0 and len(gem_hit_list) == 0:
+                    gem_placed_successfully = True
+            self.gem_list.append(gem)
+
+        for i in range(5):
+            gem = arcade.Sprite("gemBlue.png", SPRITE_SCALING)
+            gem_placed_successfully = False
+            while not gem_placed_successfully:
+                gem.center_x = random.randrange(200, 1150)
+                gem.center_y = 325
+                wall_hit_list = arcade.check_for_collision_with_list(gem, self.wall_list)
+                gem_hit_list = arcade.check_for_collision_with_list(gem, self.gem_list)
+                if len(wall_hit_list) == 0 and len(gem_hit_list) == 0:
+                    gem_placed_successfully = True
+            self.gem_list.append(gem)
+
+        for i in range(5):
+            gem = arcade.Sprite("gemBlue.png", SPRITE_SCALING)
+            gem_placed_successfully = False
+            while not gem_placed_successfully:
+                gem.center_x = random.randrange(200, 1150)
+                gem.center_y = 475
+                wall_hit_list = arcade.check_for_collision_with_list(gem, self.wall_list)
+                gem_hit_list = arcade.check_for_collision_with_list(gem, self.gem_list)
+                if len(wall_hit_list) == 0 and len(gem_hit_list) == 0:
+                    gem_placed_successfully = True
+            self.gem_list.append(gem)
+
+        for i in range(5):
+            gem = arcade.Sprite("gemBlue.png", SPRITE_SCALING)
+            gem_placed_successfully = False
+            while not gem_placed_successfully:
+                gem.center_x = random.randrange(200, 1150)
+                gem.center_y = 625
+                wall_hit_list = arcade.check_for_collision_with_list(gem, self.wall_list)
+                gem_hit_list = arcade.check_for_collision_with_list(gem, self.gem_list)
+                if len(wall_hit_list) == 0 and len(gem_hit_list) == 0:
+                    gem_placed_successfully = True
+            self.gem_list.append(gem)
+
+        for i in range(2):
+            gem = arcade.Sprite("gemBlue.png", SPRITE_SCALING)
+            gem_placed_successfully = False
+            while not gem_placed_successfully:
+                gem.center_x = random.randrange(200, 1150)
+                gem.center_y = 750
+                wall_hit_list = arcade.check_for_collision_with_list(gem, self.wall_list)
+                gem_hit_list = arcade.check_for_collision_with_list(gem, self.gem_list)
+                if len(wall_hit_list) == 0 and len(gem_hit_list) == 0:
+                    gem_placed_successfully = True
+            self.gem_list.append(gem)
+
+        # Apply the physics engine.
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
         # Set the background color
         arcade.set_background_color(arcade.color.DARK_GREEN)
 
     def on_draw(self):
+        """
+        Draw the walls, sprites, score, and gems, and use the camera.
+        """
         arcade.start_render()
 
         self.camera_sprites.use()
@@ -183,9 +267,7 @@ class MyGame(arcade.Window):
 
         arcade.draw_rectangle_filled(self.width // 2, 20, self.width, 40, arcade.color.ALMOND)
 
-        text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, " \
-               f"{self.camera_sprites.position[1]:5.1f})"
-        arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
+        arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.BLACK_BEAN, 20)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -208,14 +290,27 @@ class MyGame(arcade.Window):
             self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
-
+        """Update lists and create and add to the gem_hit_list."""
         self.physics_engine.update()
-
+        self.gem_list.update()
         self.player_list.update()
 
-        lower_left_corner = (self.player_sprite.center_x - self.width / 2,
-                             self.player_sprite.center_y - self.height / 2)
-        self.camera_sprites.move_to(lower_left_corner, CAMERA_SPEED)
+        gem_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.gem_list)
+
+        for gem in gem_hit_list:
+            gem.remove_from_sprite_lists()
+            self.score += 1
+            arcade.play_sound(self.gem_sound)
+
+        self.scroll_to_player()
+
+    def scroll_to_player(self):
+        """
+        Scroll the window to the player.
+        """
+        position = self.player_sprite.center_x - self.width / 2, \
+            self.player_sprite.center_y - self.height / 2
+        self.camera_sprites.move_to(position, CAMERA_SPEED)
 
     def on_resize(self, width, height):
         """
